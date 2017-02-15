6ee81bb8f56026128e9900181bd41fc0ed99c7e2
Austin McInnis <austin.mcinnis@icloud.com>
2017-02-15 14:51:52 -0800

    Initial commit.

---
+++ Gradebook/LoginViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LoginViewController.swift
+//  Gradebook
+//
+//  Created by Austin McInnis on 2/14/17.
+//  Copyright © 2017 Austin McInnis. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var urlTextField: UITextField!
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func testLogin(_ sender: Any) {
+        urlTextField.text = "https://users.csc.calpoly.edu/~bellardo/cgi-bin/test/grades.json"
+        loginTextField.text = "test"
+        passwordTextField.text = "fSxgQfMdm6"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
